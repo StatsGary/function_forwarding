95849e024ee06d6e8f0c23cdf5f448d569f65c93
Gary Hutson <StatsGary@users.noreply.github.com>
2020-04-24 13:03:13 +0100

    Finished: http://adv-r.had.co.nz/Functions.html tutorial

--- Advanced_R_Functions.R
+++ Advanced_R_Functions.R
@@ -310,5 +310,218 @@ add <- function(x) {
 }
 #because the force function is defined as force <- function(x) x. However, using this function clearly indicates that you're forcing evaluation, not that you've accidentally typed x.
 #Default arguments are evaluated inside the function. This means that if the expression depends on the current environment the results will differ depending on whether you use the default value or explicitly provide one.
+f <- function(x = ls()) {
+  a <- 1
+  x
+}
 
+f()
+f(ls())
+
+#More technically, an unevaluated argument is called a promise, or (less commonly) a thunk. A promise is made up of two parts:
+#The expression which gives rise to the delayed computation. (It can be accessed with substitute(). See non-standard evaluation for more details.)
+#The environment where the expression was created and where it should be evaluated.
+#The first time a promise is accessed the expression is evaluated in the environment where it was created. This value is cached, so that subsequent access to the evaluated promise does not recompute the value (but the original expression is still associated with the value, so substitute() can continue to access it). You can find more information about a promise using pryr::promise_info(). This uses some C++ code to extract information about the promise without evaluating it, which is impossible to do in pure R code.
+#Laziness is useful in if statements - the second statement below will be evaluated only if the first is true. If it wasn't, the statement would return an error because NULL > 0 is a logical vector of length 0 and not a valid input to if.
+
+
+x <- NULL
+if(!is.null(x) && x > 0){
+  
+}
+
+# We could implement the && function ourselves
+
+'&&' <- function(x,y){
+  if(!x) return(FALSE)
+  if(!y) return(FALSE)
+  
+  TRUE
+}
+
+a <- NULL
+!is.null(a) && a > 0
+
+#This function would not work without lazy evaluation because both x and y would always be evaluated, testing a > 0 even when a was NULL.
+#Sometimes you can also use laziness to eliminate an if statement altogether. For example, instead of:
+
+if (is.null(a)) stop("a is null")
+
+!is.null(a) || stop("a is null")
+
+
+# DOT DOT DOT function....
+#There is a special argument called ... . This argument will match any arguments not otherwise matched, and can be easily passed on to other functions. This is useful if you want to collect arguments to call another function, but you don't want to prespecify their possible names. ... is often used in conjunction with S3 generic functions to allow individual methods to be more flexible.
+#One relatively sophisticated user of ... is the base plot() function. plot() is a generic method with arguments x, y and ... . To understand what ... does for a given function we need to read the help: "Arguments to be passed to methods, such as graphical parameters". Most simple invocations of plot() end up calling plot.default() which has many more arguments, but also has ... . Again, reading the documentation reveals that ... accepts "other graphical parameters", which are listed in the help for par(). This allows us to write code like:
+
+plot(1:5, col = "red")
+plot(1:5, cex = 5, pch = 20)
+
+#This illustrates both the advantages and disadvantages of ...: it makes plot() very flexible, but to understand how to use it, we have to carefully read the documentation. Additionally, if we read the source code for plot.default, we can discover undocumented features. It's possible to pass along other arguments to Axis() and box():
+#To capture ... in a form that is easier to work with, you can use list(...). (See capturing unevaluated dots for other ways to capture ... without evaluating the arguments.)
+f <- function(...) {
+  names(list(...))
+}
+f(a = 1, b = 2)
+
+
+#------------------------- Special Calls ------------------------------#
+#R supports two additional syntaxes for calling special types of functions: infix and replacement functions.
+
+
+#Infix functions
+#Most functions in R are "prefix" operators: the name of the function comes before the arguments. You can also create infix functions where the function name comes in between its arguments, like + or -. All user-created infix functions must start and end with %. R comes with the following infix functions predefined: %%, %*%, %/%, %in%, %o%, %x%. (The complete list of built-in infix operators that don't need % is: :, ::, :::, $, @, ^, *, /, +, -, >, >=, <, <=, ==, !=, !, &, &&, |, ||, ~, <-, <<-)
+
+`%+%` <- function(a, b) paste0(a, b)
+"new" %+% " string"
+#Note that when creating the function, you have to put the name in backticks because it's a special name. This is just a syntactic sugar for an ordinary function call; as far as R is concerned there is no difference between these two expressions:
+"new" %+% " string"
+`%+%`("new", " string")
+
+#The names of infix functions are more flexible than regular R functions: they can contain any sequence of characters (except "%", of course). You will need to escape any special characters in the string used to define the function, but not when you call it:7
+
+`% %` <- function(a, b) paste(a, b)
+`%'%` <- function(a, b) paste(a, b)
+`%/\\%` <- function(a, b) paste(a, b)
+"a" % % "b"
+"a" %'% "b"
+"a" %/\% "b"
+
+#R's default precedence rules mean that infix operators are composed from left to right:
+`%-%` <- function(a, b) paste0("(", a, " %-% ", b, ")")
+"a" %-% "b" %-% "c"
+
+#There's one infix function that I use very often. It's inspired by Ruby's || logical or operator, although it works a little differently in R because Ruby has a more flexible definition of what evaluates to TRUE in an if statement. It's useful as a way of providing a default value in case the output of another function is NULL:
+
+#`%||%` <- function(a, b) if (!is.null(a)) a else b
+#function_that_might_return_null() %||% default value
+
+#Replacement functions
+# functions act like they modify their arguments in place, and have the special name xxx<-. They typically have two arguments (x and value), although they can have more, and they must return the modified object. For example, the following function allows you to modify the second element of a vector:
+
+`second<-` <- function(x, value) {
+  x[2] <- value
+  x
+}
+
+x <- 1:10
+second(x) <- 5L
+x
+
+#When R evaluates the assignment second(x) <- 5, it notices that the left hand side of the <- is not a simple name, so it looks for a function named second<- to do the replacement.
+
+#I say they "act" like they modify their arguments in place, because they actually create a modified copy. We can see that by using pryr::address() to find the memory address of the underlying object.
+
+library(pryr)
+x <- 1:10
+address(x)
+second(x) <- 6L
+address(x)
+
+
+`modify<-` <- function(x, position, value) {
+  x[position] <- value
+  x
+}
+modify(x, 1) <- 10
+x
+
+#When you call modify(x, 1) <- 10, behind the scenes R turns it into:
+
+x <- `modify<-`(x, 1, 10)
+x
+
+#It's often useful to combine replacement and subsetting:
+x <- c(a = 1, b = 2, c = 3)
+names(x)
+names(x)[2] <- "two"
+names(x)
+
+`*tmp*` <- names(x)
+`*tmp*`[2] <- "two"
+names(x) <- `*tmp*`
+
+#------------------------------Return values
+#The last expression evaluated in a function becomes the return value, the result of invoking the function.
+f <- function(x) {
+  if (x < 10) {
+    0
+  } else {
+    10
+  }
+}
+
+f(5)
+f(15)
+#Generally, I think it's good style to reserve the use of an explicit return() for when you are returning early, such as for an error, or a simple case of the function. This style of programming can also reduce the level of indentation, and generally make functions easier to understand because you can reason about them locally.
+
+f <- function(x, y) {
+  if (!x) return(y)
+  
+  # complicated processing here
+}
+
+f(y=10)
+
+#R protects you from one type of side effect: most R objects have copy-on-modify semantics. So modifying a function argument does not change the original value:
+f <- function(x) {
+  x$a <- 2
+  x
+}
+x <- list(a = 1)
+f(x)
+
+
+#(There are two important exceptions to the copy-on-modify rule: environments and reference classes. These can be modified in place, so extra care is needed when working with them.)
+#This is notably different to languages like Java where you can modify the inputs of a function. This copy-on-modify behaviour has important performance consequences which are discussed in depth in profiling. (Note that the performance consequences are a result of R's implementation of copy-on-modify semantics; they are not true in general. Clojure is a new language that makes extensive use of copy-on-modify semantics with limited performance consequences.)
+
+#Most base R functions are pure, with a few notable exceptions:
+#library() which loads a package, and hence modifies the search path.
+#setwd(), Sys.setenv(), Sys.setlocale() which change the working directory, environment variables, and the locale, respectively.
+#plot() and friends which produce graphical output.
+#write(), write.csv(), saveRDS(), etc. which save output to disk.
+#options() and par() which modify global settings.
+#S4 related functions which modify global tables of classes and methods.
+#Random number generators which produce different numbers each time you run them.
+#s generally a good idea to minimise the use of side effects, and where possible, to minimise the footprint of side effects by separating pure from impure functions. Pure functions are easier to test (because all you need to worry about are the input values and the output), and are less likely to work differently on different versions of R or on different platforms. For example, this is one of the motivating principles of ggplot2: most operations work on an object that represents a plot, and only the final print or plot call has the side effect of actually drawing the plot.
+#Functions can return invisible values, which are not printed out by default when you call the function.
+
+f1 <- function() 1
+f2 <- function() invisible(1)
+
+f1()
+f2()
+f1() == 1
+f2() == 1
+#You can force an invisible value to be displayed by wrapping it in parentheses:
+
+(f2())
+
+#The most common function that returns invisibly is <-:
+a <- 2
+(a <- 2)
+#This is what makes it possible to assign one value to multiple variables:
+#a <- b <- c <- d <- 2
+(a <- (b <- (c <- (d <- 2))))
+
+# On exit
+
+#As well as returning a value, functions can set up other triggers to occur when the function is finished using on.exit(). This is often used as a way to guarantee that changes to the global state are restored when the function exits. The code in on.exit() is run regardless of how the function exits, whether with an explicit (early) return, an error, or simply reaching the end of the function body.
+
+in_dir <- function(dir, code) {
+  old <- setwd(dir)
+  on.exit(setwd(old))
+  
+  force(code)
+}
+
+getwd()
+in_dir("~", getwd())
+
+#The basic pattern is simple:
+#We first set the directory to a new location, capturing the current location from the output of setwd().
+#We then use on.exit() to ensure that the working directory is returned to the previous value regardless of how the function exits.
+#Finally, we explicitly force evaluation of the code. (We don't actually need force() here, but it makes it clear to readers what we're doing.)
+
+#Caution: If you're using multiple on.exit() calls within a function, make sure to set add = TRUE. Unfortunately, the default in on.exit() is add = FALSE, so that every time you run it, it overwrites existing exit expressions. Because of the way on.exit() is implemented, it's not possible to create a variant with add = TRUE, so you must be careful when using it.
 
